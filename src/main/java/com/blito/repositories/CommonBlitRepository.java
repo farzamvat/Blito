5b6611d8ed5d93b94f992609222e60f4e328800d
Hasti <hasti.sahabi@gmail.com>
2017-05-20 19:11:32 +0430

    AdminEventServiceTest Ok

---
+++ src/main/java/com/blito/repositories/CommonBlitRepository.java
@@ -0,0 +1,7 @@
+package com.blito.repositories;
+
+import com.blito.models.CommonBlit;
+
+public interface CommonBlitRepository extends BlitBaseRepository<CommonBlit>{
+
+}
--- src/main/java/com/blito/services/AdminEventService.java
+++ src/main/java/com/blito/services/AdminEventService.java
@@ -8,12 +8,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.blito.enums.OperatorState;
 import com.blito.enums.Response;
 import com.blito.exceptions.NotFoundException;
 import com.blito.mappers.AdminReportsMapper;
 import com.blito.mappers.EventFlatMapper;
+import com.blito.mappers.EventMapper;
 import com.blito.models.CommonBlit;
 import com.blito.models.Event;
 import com.blito.models.EventDate;
@@ -24,6 +25,7 @@ import com.blito.rest.viewmodels.adminreport.BlitBuyerViewModel;
 import com.blito.rest.viewmodels.event.AdminChangeEventOperatorStateVm;
 import com.blito.rest.viewmodels.event.AdminChangeEventStateVm;
 import com.blito.rest.viewmodels.event.EventFlatViewModel;
+import com.blito.rest.viewmodels.event.EventViewModel;
 
 @Service
 public class AdminEventService {
@@ -33,7 +35,9 @@ public class AdminEventService {
 	@Autowired
 	EventDateRepository eventDateRepository;
 	@Autowired
-	EventFlatMapper eventMapper;
+	EventFlatMapper eventFlatMapper;
+	@Autowired
+	EventMapper eventMapper;
 	@Autowired
 	AdminReportsMapper adminReportsMapper;
 
@@ -42,44 +46,44 @@ public class AdminEventService {
 				.orElseThrow(() -> new NotFoundException(ResourceUtil.getMessage(Response.EVENT_NOT_FOUND)));
 		return event;
 	}
-
+	@Transactional
 	public void changeEventState(AdminChangeEventStateVm vmodel) {
 		Event event = getEventFromRepository(vmodel.getEventId());
 		event.setEventState(vmodel.getState());
-		eventRepository.save(event);
 		return;
 	}
 
+	@Transactional
 	public void changeOperatorState(AdminChangeEventOperatorStateVm vmodel) {
 		Event event = getEventFromRepository(vmodel.getEventId());
 		event.setOperatorState(vmodel.getOperatorState());
-		eventRepository.save(event);
 		return;
 	}
 	
+	@Transactional
 	public void setEventOrderNumber(long eventId, int order) {
 		Event event = getEventFromRepository(eventId);
 		event.setOrderNumber(order);
-		eventRepository.save(event);
 		return;
 	}
 
 	public Page<EventFlatViewModel> getAllEvents(Pageable page) {
-		return eventMapper.toPage(eventRepository.findAll(page), eventMapper::createFromEntity);
+		return eventFlatMapper.toPage(eventRepository.findAll(page), eventFlatMapper::createFromEntity);
 	}
 
-	// ViewModel Issues!!
 	public EventFlatViewModel getEvent(long eventId) {
 		Event event = getEventFromRepository(eventId);
-		return eventMapper.createFromEntity(event);
+		return eventFlatMapper.createFromEntity(event);
 	}
 
-	public EventFlatViewModel updateEvent(EventFlatViewModel vmodel) {
+	@Transactional
+	public EventFlatViewModel updateEvent(EventViewModel vmodel) {
 		Event event = getEventFromRepository(vmodel.getEventId());
-		return eventMapper
-				.createFromEntity(eventRepository.save(eventMapper.updateEntity(vmodel, event)));
+		return eventFlatMapper
+				.createFromEntity(eventMapper.updateEntity(vmodel, event));
 	}
 
+	@Transactional
 	public Page<BlitBuyerViewModel> getEventBlitBuyersByEventDate(long eventDateId, Pageable pageable) {
 		EventDate eventDate = Optional.ofNullable(eventDateRepository.findOne(eventDateId)).map(ed -> ed)
 				.orElseThrow(() -> new NotFoundException(ResourceUtil.getMessage(Response.EVENT_DATE_NOT_FOUND)));
