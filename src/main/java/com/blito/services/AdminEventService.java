69930d65624a4fd3b4434298f9c53da3e731e69c
Hasti <hasti.sahabi@gmail.com>
2017-05-29 13:40:24 +0430

    admin set discount code added

--- src/main/java/com/blito/services/AdminEventService.java
+++ src/main/java/com/blito/services/AdminEventService.java
@@ -11,21 +11,31 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.blito.enums.Response;
+import com.blito.exceptions.AlreadyExistsException;
+import com.blito.exceptions.InconsistentDataException;
 import com.blito.exceptions.NotFoundException;
 import com.blito.mappers.AdminReportsMapper;
+import com.blito.mappers.DiscountMapper;
 import com.blito.mappers.EventFlatMapper;
 import com.blito.mappers.EventMapper;
+import com.blito.models.BlitType;
 import com.blito.models.CommonBlit;
+import com.blito.models.Discount;
 import com.blito.models.Event;
 import com.blito.models.EventDate;
+import com.blito.repositories.BlitTypeRepository;
+import com.blito.repositories.DiscountRepository;
 import com.blito.repositories.EventDateRepository;
 import com.blito.repositories.EventRepository;
+import com.blito.repositories.UserRepository;
 import com.blito.resourceUtil.ResourceUtil;
 import com.blito.rest.viewmodels.adminreport.BlitBuyerViewModel;
+import com.blito.rest.viewmodels.discount.DiscountViewModel;
 import com.blito.rest.viewmodels.event.AdminChangeEventOperatorStateVm;
 import com.blito.rest.viewmodels.event.AdminChangeEventStateVm;
 import com.blito.rest.viewmodels.event.EventFlatViewModel;
 import com.blito.rest.viewmodels.event.EventViewModel;
+import com.blito.security.SecurityContextHolder;
 
 @Service
 public class AdminEventService {
@@ -40,7 +50,17 @@ public class AdminEventService {
 	EventMapper eventMapper;
 	@Autowired
 	AdminReportsMapper adminReportsMapper;
-
+	@Autowired
+	DiscountMapper discountMapper;
+	@Autowired
+	DiscountRepository discountRepository;
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	BlitTypeRepository blitTypeRepository;
+	
+	
+	
 	public Event getEventFromRepository(long eventId) {
 		Event event = Optional.ofNullable(eventRepository.findOne(eventId)).map(e -> e)
 				.orElseThrow(() -> new NotFoundException(ResourceUtil.getMessage(Response.EVENT_NOT_FOUND)));
@@ -92,6 +112,27 @@ public class AdminEventService {
 				.flatMap(bt -> bt.getCommonBlits().stream()).skip(pageable.getPageNumber() * pageable.getPageSize())
 				.limit(pageable.getPageSize()).collect(Collectors.toList()));
 		return adminReportsMapper.toPage(page, adminReportsMapper::toBlitBuyerReport);
+	}
+	
+	@Transactional
+	public BlitType getBlitTypeFromRepository(long blitTypeId) {
+		return Optional.ofNullable(blitTypeRepository.findOne(blitTypeId)).map(e -> e)
+				.orElseThrow(() -> new NotFoundException(ResourceUtil.getMessage(Response.BLIT_TYPE_NOT_FOUND)));
+	}
+	
+	@Transactional
+	public DiscountViewModel setDiscountCode(DiscountViewModel vmodel) {
+		if (vmodel.getEffectDate().after(vmodel.getExpirationDate()))
+			throw new InconsistentDataException(ResourceUtil.getMessage(Response.INCONSISTENT_DATES));
+		if(discountRepository.findByCode(vmodel.getCode()).isPresent())
+			throw new AlreadyExistsException(ResourceUtil.getMessage(Response.DISCOUNT_CODE_ALREADY_EXISTS));
+		Discount discount = discountMapper.createFromViewModel(vmodel);
+		discount.setUser(userRepository.findOne(SecurityContextHolder.currentUser().getUserId()));
+		discount.setBlitTypes(
+				vmodel.getBlitTypeIds().stream().map(bt -> getBlitTypeFromRepository(bt)).collect(Collectors.toList()));
+		
+		discount = discountRepository.save(discount);
+		return discountMapper.createFromEntity(discount);
 	}
 
 }
