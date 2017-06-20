ccaaad3b445642fe64d0a03514b5f0697bbcd391
Vat <farzam.vat@gmail.com>
2017-06-20 16:56:54 +0430

    commit for sahab

--- src/main/java/com/blito/payments/localhost/_8085/services/FinalizePaymentRequest.java
+++ src/main/java/com/blito/payments/localhost/_8085/services/FinalizePaymentRequest.java
@@ -8,6 +8,7 @@
 
 package com.blito.payments.localhost._8085.services;
 
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
