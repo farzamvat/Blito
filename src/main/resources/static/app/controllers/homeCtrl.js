17e5bf2474fa9487f5be65c6ca01f419b4049879
soroush yousefi <soroush.ysf@gmail.com>
2017-06-30 21:28:03 +0430

    home page mini slider get data

--- src/main/resources/static/app/controllers/homeCtrl.js
+++ src/main/resources/static/app/controllers/homeCtrl.js
@@ -3,7 +3,79 @@
  */
 
 angular.module('homePageModule', [])
-    .controller('homeCtrl', function () {
+    .controller('homeCtrl', function ($scope, miniSliderService, photoService) {
+        $scope.concertRow = [];
+        miniSliderService.getSlidingDataEvents("CONCERT", 6, false)
+            .then(function (data, status) {
+                $scope.concertRow = $scope.catchImagesEvents(data.data.content);
+            })
+            .catch(function (data, status) {
+                console.log(data);
+            })
+        ;
+        miniSliderService.getSlidingDataEvents("TOURISM", 6, false)
+            .then(function (data, status) {
+                $scope.tourRow = $scope.catchImagesEvents(data.data.content);
+            })
+            .catch(function (data, status) {
+                console.log(data);
+            })
+        miniSliderService.getSlidingDataEvents("evento", 6, true)
+            .then(function (data, status) {
+                $scope.evento = $scope.catchImagesEvents(data.data.content);
+            })
+            .catch(function (data, status) {
+                console.log(data);
+            })
+        miniSliderService.getSlidingDataExchange(6)
+            .then(function (data, status) {
+                $scope.exchange = $scope.catchImagesExchange(data.data.content);
+            })
+            .catch(function (data, status) {
+                console.log(data);
+            })
+        miniSliderService.getEndedEvents(6)
+            .then(function (data, status) {
+                console.log(data);
+                $scope.ended = $scope.catchImagesEvents(data.data.content);
+            })
+            .catch(function (data, status) {
+                console.log(data);
+            })
+        $scope.catchImagesEvents = function (events) {
+            events.map(function (item) {
+                var newSet =  item.images.filter(function (image) {
+                    return image.type === "EVENT_PHOTO";
+                });
+                newSet.map(function (image) {
+                    return photoService.download(image.imageUUID)
+                        .then(function (data, status) {
+                            image.imageUUID = data.data.encodedBase64;
+                            return image;
+                        })
+                        .catch(function (data, status) {
+                            console.log(data);
+                        })
+                });
+                item.images = newSet;
+                return item;
 
+            });
+            return events;
+        }
+        $scope.catchImagesExchange = function (events) {
+            events.map(function (item) {
+                     photoService.download(item.image.imageUUID)
+                        .then(function (data, status) {
+                           item.image.imageUUID = data.data.encodedBase64;
+                            return item.image;
+                        })
+                        .catch(function (data, status) {
+                            console.log(data);
+                        })
+                return item;
 
+            });
+            return events;
+        }
     });
---
+++ src/main/resources/static/app/services/home-page-services.js
@@ -0,0 +1,63 @@
+/**
+ * Created by soroush on 6/30/17.
+ */
+
+angular.module('homePageApi', [])
+    .service('miniSliderService', function ($http, config) {
+        var miniSlider = this;
+        miniSlider.getSlidingDataEvents = function (eventType, size, evento) {
+            var queryParam = {
+                params : {page: 0, size: size, sort: "orderNumber,desc"}
+            };
+            if(!evento) {
+                var bodyJson = {
+                    restrictions: [
+                        {field: "isDeleted", type: "simple", operation: "eq", value: "false"},
+                        {field: "eventState", type: "simple", operation: "neq", value: "ENDED"},
+                        {field: "operatorState", type: "simple", operation: "eq", value: "APPROVED"},
+                        {field: "eventType", type: "simple", operation: "eq", value: eventType}
+
+                    ]
+                };
+            } else if(evento) {
+                var bodyJson = {
+                    restrictions: [
+                        {field: "isDeleted", type: "simple", operation: "eq", value: "false"},
+                        {field: "eventState", type: "simple", operation: "neq", value: "ENDED"},
+                        {field: "operatorState", type: "simple", operation: "eq", value: "APPROVED"},
+                        {field: "isEvento", type: "simple", operation: "eq", value: "true"}
+
+                    ]
+                };
+            }
+            return $http.post(config.baseUrl + '/api/blito/v1.0/public/events/search',bodyJson, queryParam);
+
+        }
+        miniSlider.getSlidingDataExchange = function (size) {
+            var queryParam = {
+                params : {page: 0, size: size, sort: "createdAt,desc"}
+            };
+                var bodyJson = {
+                    restrictions: [
+                        {field: "isDeleted", type: "simple", operation: "eq", value: "false"},
+                        {field: "operatorState", type: "simple", operation: "eq", value: "APPROVED"}
+                    ]
+                };
+
+            return $http.post(config.baseUrl + '/api/blito/v1.0/public/exchange-blits/search',bodyJson, queryParam);
+        }
+        miniSlider.getEndedEvents = function (size) {
+            var queryParam = {
+                params : {page: 0, size: size, sort: "createdAt,desc"}
+            };
+            var bodyJson = {
+                restrictions: [
+                    {field: "isDeleted", type: "simple", operation: "eq", value: "false"},
+                    {field: "eventState", type: "simple", operation: "eq", value: "ENDED"},
+                    {field: "operatorState", type: "simple", operation: "eq", value: "APPROVED"}
+                    ]
+            };
+
+            return $http.post(config.baseUrl + '/api/blito/v1.0/public/events/search',bodyJson, queryParam);
+        }
+    })
