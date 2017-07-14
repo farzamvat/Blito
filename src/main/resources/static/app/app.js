2e07601f5b4e57303d0dc2ef82ef7a34976c2119
soroush yousefi <soroush.ysf@gmail.com>
2017-07-14 18:17:08 +0430

    ip

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -25,7 +25,7 @@ angular.module('Blito',
         $httpProvider.interceptors.push('AuthInterceptors');
     })
     .constant('config', {
-        baseUrl : 'http://213.32.14.78',
+        baseUrl : 'http://http://213.32.14.78',
         redirectToUrlAfterLogin : {url : '/'}
     })
 ;
