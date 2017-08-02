6c503bba579e3e6a98de78042602dec05d865c84
Vat <farzam.vat@gmail.com>
2017-08-02 20:06:52 +0430

    sms service download link

--- src/main/java/com/blito/services/SmsService.java
+++ src/main/java/com/blito/services/SmsService.java
@@ -11,18 +11,23 @@ import org.springframework.web.client.RestTemplate;
 public class SmsService {
 
 	@Value("${kaveh.negar.api.key}")
-	String apiKey;
+	private String apiKey;
+	@Value("${serverAddress}")
+	private String serverAddress;
+	@Value("${api.base.url}")
+	private String baseUrl;
 
 	RestTemplate rest = new RestTemplate();
 
 	private final Logger log = LoggerFactory.getLogger(SmsService.class);
-	
+
 	public void sendBlitRecieptSms(String receptor, String token) {
-		
-		ResponseEntity<String> response = rest.getForEntity("https://api.kavenegar.com/v1/" + apiKey
-				+ "/verify/lookup.json?receptor=" + receptor + "&token=" + token +"&template=" + "BlitoTrackCode",
+
+		ResponseEntity<String> response = rest.getForEntity(
+				"https://api.kavenegar.com/v1/" + apiKey + "/verify/lookup.json?receptor=" + receptor + "&token="
+						+ serverAddress + baseUrl + "/public/blits/" + token + "/blit.pdf" + "&template=" + "BlitoTrackCode",
 				String.class);
-		
+
 		log.debug(response.getBody());
 
 	}
