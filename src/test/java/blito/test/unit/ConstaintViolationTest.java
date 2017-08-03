59ac2f7836ae4e61b165f777d787ef0bb6ff719d
Vat <farzam.vat@gmail.com>
2017-08-03 13:16:08 +0430

    constraint violationTest

---
+++ src/test/java/blito/test/unit/ConstaintViolationTest.java
@@ -0,0 +1,49 @@
+package blito.test.unit;
+
+
+import static org.junit.Assert.*;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.blito.Application;
+import com.blito.models.User;
+import com.blito.repositories.UserRepository;
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=Application.class)
+public class ConstaintViolationTest {
+
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	@Test
+	public void exceptionTest()
+	{
+		User user = new User();
+		user.setEmail("farzam.vat@gmail.com");
+		user.setPassword("12345678");
+		userRepository.save(user);
+		
+		try {
+
+			User user1 = new User();
+			user1.setEmail("farzam.vat@gmail.com");
+			user1.setPassword("12345678");
+			userRepository.save(user1);
+			assertTrue(true);
+		}
+		catch(Exception e)
+		{
+			System.err.println(e);
+			assertTrue(false);
+		}
+	}
+}
