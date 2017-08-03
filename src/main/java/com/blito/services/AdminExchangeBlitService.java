8c58bbe8d2b1c6985bd9048a9dd4640ec30b1b7a
Vat <farzam.vat@gmail.com>
2017-08-04 00:02:19 +0430

    delete exchangeBlit persistance problem

--- src/main/java/com/blito/services/AdminExchangeBlitService.java
+++ src/main/java/com/blito/services/AdminExchangeBlitService.java
@@ -49,5 +49,6 @@ public class AdminExchangeBlitService {
 				.orElseThrow(() -> new NotFoundException(ResourceUtil.getMessage(Response.BLIT_NOT_FOUND)));
 		imageService.delete(exchangeBlit.getImage().getImageUUID());
 		exchangeBlit.setDeleted(true);
+		exchangeBlitRepository.save(exchangeBlit);
 	}
 }
