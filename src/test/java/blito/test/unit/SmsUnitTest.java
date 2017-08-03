498707623844982aa0c89f15e72c0a5708ee3cb9
Vat <farzam.vat@gmail.com>
2017-08-03 12:42:55 +0430

    url shortener added for sending sms

--- src/test/java/blito/test/unit/SmsUnitTest.java
+++ src/test/java/blito/test/unit/SmsUnitTest.java
@@ -2,6 +2,7 @@ package blito.test.unit;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -10,6 +11,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.blito.Application;
+import com.blito.services.SmsService;
 
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
@@ -18,18 +20,22 @@ public class SmsUnitTest {
 	
 	@Value("${kaveh.negar.api.key}")
 	String apiKey;
+	@Autowired
+	private SmsService smsService;
 	
 	TestRestTemplate rest = new TestRestTemplate();
 	
 	@Test
-	public void sendSmsKaveNegar()
+	public void sendSmsKaveNegar() throws InterruptedException
 	{
-
-		ResponseEntity<String> response = rest.getForEntity("https://api.kavenegar.com/v1/"+ apiKey + "/verify/lookup.json?receptor=09127976837&token=852596&template=Blito", String.class);
-		System.out.println("******************************");
-		System.out.println(response.getBody());
-		System.out.println("******************************");
-
+//
+//		ResponseEntity<String> response = rest.getForEntity("https://api.kavenegar.com/v1/"+ apiKey + "/verify/lookup.json?receptor=09127976837&token=852596&template=Blito", String.class);
+//		System.out.println("******************************");
+//		System.out.println(response.getBody());
+//		System.out.println("******************************");
+//		
 		
+		smsService.sendBlitRecieptSms("09124337522", "32816213");
+		Thread.sleep(5000);
 	}
 }
