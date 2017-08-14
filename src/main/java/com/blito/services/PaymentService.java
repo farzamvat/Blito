06e83d27f5e61c4dc4e79786f3ae0c82cf56ad4d
Vat <farzam.vat@gmail.com>
2017-08-14 16:08:12 +0430

    save common blit after payment

--- src/main/java/com/blito/services/PaymentService.java
+++ src/main/java/com/blito/services/PaymentService.java
@@ -128,7 +128,7 @@ public class PaymentService {
 						Timestamp.from(ZonedDateTime.now(ZoneId.of("Asia/Tehran")).toInstant()));
 			}
 		}
-		return commonBlit;
+		return commonBlitRepository.save(commonBlit);
 	}
 	
 	@Transactional(propagation = Propagation.REQUIRES_NEW, isolation = Isolation.SERIALIZABLE)
