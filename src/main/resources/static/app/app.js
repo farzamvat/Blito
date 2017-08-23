de4fe6f7104497c0f099bf3a96013ba20595f1b3
soroush yousefi <soroush.ysf@gmail.com>
2017-08-23 16:38:36 +0430

    cache update

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -31,7 +31,6 @@ angular.module('Blito',
         if (!$httpProvider.defaults.headers.get) {
             $httpProvider.defaults.headers.get = {};
         }
-        // $httpProvider.defaults.cache = true;
         $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
         $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
         $analyticsProvider.settings.ga.account = 'UA-103668411-1';
