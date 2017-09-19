d16cac135ea605ba36bbf772c493b409cf9d6859
Vat <farzam.vat@gmail.com>
2017-09-19 19:29:27 +0430

    mail changed to blito.ir from bili.to. url shortener bug fixed

--- src/main/java/com/blito/services/SmsService.java
+++ src/main/java/com/blito/services/SmsService.java
@@ -1,9 +1,11 @@
 package com.blito.services;
 
+import com.blito.rest.viewmodels.google.shortener.UrlShortenerResponseViewModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -26,18 +28,11 @@ public class SmsService {
 	public void sendBlitRecieptSms(String receptor, String token) {
 
 		String blitPdfUrl = String.valueOf(new StringBuilder(serverAddress).append(baseUrl).append("/public/blits/").append(token).append("/blit.pdf"));
-		
-		urlShortenerService.generateShortenUrl(blitPdfUrl)
-			.thenApply(urlShortenerResponse -> {
-				return rest.getForEntity(
-						"https://api.kavenegar.com/v1/" + apiKey + "/verify/lookup.json?receptor=" + receptor + "&token="
-								+ urlShortenerResponse.getId() + "&template=" + "BlitoReceipt",
-						String.class);
-			}).handle((response,throwable) -> {
-				if(throwable != null)
-					log.error("Error in sending sms to kavenegar '{}'",throwable);
-				log.debug("sending sms to kavenegar response '{}'",response.getBody());
-				return "";
-			});
+		UrlShortenerResponseViewModel urlShortenerResponseViewModel = urlShortenerService.generateShortenUrl(blitPdfUrl);
+		ResponseEntity<String> smsResponse = rest.getForEntity(
+				"https://api.kavenegar.com/v1/" + apiKey + "/verify/lookup.json?receptor=" + receptor + "&token="
+						+ urlShortenerResponseViewModel.getId() + "&template=" + "BlitoReceipt",
+				String.class);
+		log.debug("sending sms to kavenegar response '{}'",smsResponse.getBody());
 	}
 }
--- src/main/java/com/blito/services/UrlShortenerService.java
+++ src/main/java/com/blito/services/UrlShortenerService.java
@@ -1,15 +1,13 @@
 package com.blito.services;
 
-import java.util.concurrent.CompletableFuture;
-
+import com.blito.rest.viewmodels.google.shortener.UrlShortenerRequestViewModel;
+import com.blito.rest.viewmodels.google.shortener.UrlShortenerResponseViewModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import com.blito.rest.viewmodels.google.shortener.UrlShortenerRequestViewModel;
-import com.blito.rest.viewmodels.google.shortener.UrlShortenerResponseViewModel;
 
 @Service
 public class UrlShortenerService {
@@ -18,18 +16,13 @@ public class UrlShortenerService {
 	private RestTemplate rest = new RestTemplate();
 	private Logger log = LoggerFactory.getLogger(getClass());
 	
-	public CompletableFuture<UrlShortenerResponseViewModel> generateShortenUrl(String url)
+	public UrlShortenerResponseViewModel generateShortenUrl(String url)
 	{
-		return CompletableFuture.supplyAsync(() -> {
-			UrlShortenerRequestViewModel requestViewModel = new UrlShortenerRequestViewModel(url);
-			return rest.postForEntity("https://www.googleapis.com/urlshortener/v1/url?key="+googleUrlShortenerApiKey, requestViewModel , UrlShortenerResponseViewModel.class);
-		}).handle((res,throwable) -> {
-			if(throwable != null)
-				log.error("Error in shortening blit pdf url '{}'",throwable);
-			UrlShortenerResponseViewModel response = res.getBody();
-			log.info("Response from google url shortener '{}'",response);
-			return response;
-		});
+		UrlShortenerRequestViewModel requestViewModel = new UrlShortenerRequestViewModel(url);
+		ResponseEntity<UrlShortenerResponseViewModel> response =
+			rest.postForEntity("https://www.googleapis.com/urlshortener/v1/url?key="+googleUrlShortenerApiKey, requestViewModel , UrlShortenerResponseViewModel.class);
+		log.info("Response from google url shortener '{}'",response);
+		return response.getBody();
 	}
 
 }
