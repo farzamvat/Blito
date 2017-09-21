2ed8a8a55c72a36008b3b7a8c17d9cb5fc3cfe19
Vat <farzam.vat@gmail.com>
2017-09-22 00:46:18 +0330

    blito.ir

--- src/main/java/com/blito/rest/controllers/BlitoErrorController.java
+++ src/main/java/com/blito/rest/controllers/BlitoErrorController.java
@@ -1,20 +1,25 @@
 package com.blito.rest.controllers;
 
-//
-//@RestController
-//public class BlitoErrorController implements ErrorController {
-//
-//	private static final String ERROR_MAPPING = "/error";
-//
-//	@Override
-//	public String getErrorPath() {
-//		return ERROR_MAPPING;
-//	}
-//
-//	@RequestMapping(path=ERROR_MAPPING)
-//	public RedirectView error()
-//	{
-//		return new RedirectView("/not-found");
-//	}
-//
-//}
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+public class BlitoErrorController implements ErrorController {
+
+	private static final String ERROR_MAPPING = "/error";
+
+	@Override
+	public String getErrorPath() {
+		return ERROR_MAPPING;
+	}
+
+	@RequestMapping(path=ERROR_MAPPING)
+	public RedirectView error()
+	{
+		return new RedirectView("/not-found");
+	}
+
+}
