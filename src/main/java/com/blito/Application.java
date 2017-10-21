48eced287d60f776a7690b434e1871231f1c4d24
Vat <farzam.vat@gmail.com>
2017-10-21 19:02:41 +0330

    samandehi validation api

--- src/main/java/com/blito/Application.java
+++ src/main/java/com/blito/Application.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +48,11 @@ public class Application {
 	public ModelAndView notFound()
 	{
 		return new ModelAndView("index");
+	}
+
+	@GetMapping("/534250.txt")
+	public ResponseEntity<String> samandehi() {
+		return ResponseEntity.ok("");
 	}
 
 	@GetMapping("/event-page/{eventLink}")
