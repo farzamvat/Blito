086dd30a6c1279190446bd9eff099ab5d85183ba
Vat <farzam.vat@gmail.com>
2017-12-02 20:59:44 +0330

    pay.ir view models added, payment request refactored

---
+++ src/main/java/com/blito/rest/viewmodels/payments/BlitoPaymentVerificationResult.java
@@ -0,0 +1,11 @@
+package com.blito.rest.viewmodels.payments;
+
+/**
+ * @author Farzam Vatanzadeh
+ * 12/2/17
+ * Mailto : farzam.vat@gmail.com
+ **/
+
+public class BlitoPaymentVerificationResult {
+
+}
