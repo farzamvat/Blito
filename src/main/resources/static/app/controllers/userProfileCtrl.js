49422dc44c3143838e6c3937e1119890de73f039
soroush yousefi <soroush.ysf@gmail.com>
2017-12-25 16:05:26 +0330

    additional field delete if no tickets sold

--- src/main/resources/static/app/controllers/userProfileCtrl.js
+++ src/main/resources/static/app/controllers/userProfileCtrl.js
@@ -1074,6 +1074,7 @@ angular.module('User')
             $scope.galleryFiveEditUUID = null;
             $scope.gallerySixEditUUID = null;
             $scope.additionalFieldsSection = false;
+            $scope.checkSoldCountForAdditionalField = false;
             document.getElementById("editEventSansSubmit").style.display = "none";
             angular.element(document.getElementsByClassName("profilePhotoUploadEditEvent"))[0].src = "";
             angular.element(document.getElementsByClassName("galleryOneEdit"))[0].src = "";
@@ -1093,7 +1094,13 @@ angular.module('User')
             } else {
                 $scope.additionalFieldsSection = true;
             }
-
+            $scope.userEventsEdit[index].eventDates.forEach(function (eventDate) {
+                eventDate.blitTypes.forEach(function (blitType) {
+                    if(blitType.soldCount > 0) {
+                        $scope.checkSoldCountForAdditionalField = true;
+                    }
+                })
+            });
             $scope.editEventFields = {
                 eventId : $scope.userEventsEdit[index].eventId,
                 eventName : $scope.userEventsEdit[index].eventName,
@@ -1902,6 +1909,9 @@ angular.module('User')
         };
         $scope.addAdditionalFieldsEdit = function () {
                 $scope.additionalFieldsEdit.push({ key : "", value : "string"});
+        };
+        $scope.deleteAdditionalFieldsEdit = function (i) {
+            $scope.additionalFieldsEdit.splice(i, 1);
         };
         //==================================================== ********* =================================
         //==================================================== PERSIAN DATE PICKER =======================
